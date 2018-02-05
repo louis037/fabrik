69d7aacad19c49f08b47dfc6e7c2d60f6d9aec63
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2018-02-05 19:52:28 +0000

    Make toolltips in individual visualisations work

--- administrator/components/com_fabrik/views/visualization/adminvisualization.js
+++ administrator/components/com_fabrik/views/visualization/adminvisualization.js
@@ -5,54 +5,61 @@
  * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
+/* jshint mootools: true */
+/* global fconsole:true, FabrikAdmin:true, Fabrik:true, PluginManager:true, Joomla:true */
+
 define(['jquery', 'admin/pluginmanager'], function (jQuery, PluginManager) {
-    var AdminVisualization = new Class({
+	var AdminVisualization = new Class({
 
-        Extends: PluginManager,
+		Extends: PluginManager,
 
-        Implements: [Options, Events],
+		Implements: [Options, Events],
 
-        options: {},
+		options: {},
 
-        initialize: function (options, lang) {
-            this.setOptions(options);
-            this.watchSelector();
-        },
+		initialize: function (options, lang) {
+			this.setOptions(options);
+			this.watchSelector();
+		},
 
-        watchSelector: function () {
-            if (typeof(jQuery) !== 'undefined') {
-                jQuery('#jform_plugin').bind('change', function (e) {
-                    this.changePlugin(e);
-                }.bind(this));
-            } else {
-                document.id('jform_plugin').addEvent('change', function (e) {
-                    e.stop();
-                    this.changePlugin(e);
-                }.bind(this));
-            }
-        },
+		watchSelector: function () {
+			if (typeof(jQuery) !== 'undefined') {
+				jQuery('#jform_plugin').bind('change', function (e) {
+					this.changePlugin(e);
+				}.bind(this));
+			} else {
+				document.id('jform_plugin').addEvent('change', function (e) {
+					e.stop();
+					this.changePlugin(e);
+				}.bind(this));
+			}
+		},
 
-        changePlugin: function (e) {
-            var myAjax = new Request({
-                url           : 'index.php',
-                'evalResponse': false,
-                'evalScripts' : function (script, text) {
-                    this.script = script;
-                }.bind(this),
-                'data'        : {
-                    'option': 'com_fabrik',
-                    'task'  : 'visualization.getPluginHTML',
-                    'format': 'raw',
-                    'plugin': e.target.get('value')
-                },
-                'update'      : document.id('plugin-container'),
-                'onComplete'  : function (r) {
-                    document.id('plugin-container').set('html', r);
-                    Browser.exec(this.script);
-                    this.updateBootStrap();
-                }.bind(this)
-            }).send();
-        }
-    });
-    return AdminVisualization;
+		changePlugin: function (e) {
+			document.id('plugin-container').empty()
+				.adopt(new Element('span').set('text', Joomla.JText._('COM_FABRIK_LOADING')));
+			var myAjax = new Request({
+				url: 'index.php',
+				'evalResponse': false,
+				'evalScripts' : function (script, text) {
+					this.script = script;
+				}.bind(this),
+				'data': {
+					'option': 'com_fabrik',
+					'task'  : 'visualization.getPluginHTML',
+					'format': 'raw',
+					'plugin': e.target.get('value')
+				},
+				'update': document.id('plugin-container'),
+				'onComplete': function (r) {
+					document.id('plugin-container').set('html', r);
+					Browser.exec(this.script);
+					this.updateBootStrap();
+					FabrikAdmin.reTip();
+				}.bind(this),
+			});
+			Fabrik.requestQueue.add(myAjax);
+		}
+	});
+	return AdminVisualization;
 });
