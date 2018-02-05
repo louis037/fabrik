8aa1428b0bc18743b0dab1f00ac15fc378d7d8e6
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2018-02-05 19:39:43 +0000

    Improve visualization backend edit...
    ... to conform to F3.1+ design (rather than bootstraped F2.5 design).

--- administrator/components/com_fabrik/views/visualization/tmpl/bootstrap.php
+++ administrator/components/com_fabrik/views/visualization/tmpl/bootstrap.php
@@ -4,9 +4,9 @@
  *
  * @package     Joomla.Administrator
  * @subpackage  Fabrik
- * @copyright   Copyright (C) 2005-2016  Media A-Team, Inc. - All rights reserved.
+ * @copyright   Copyright (C) 2005-2018  Media A-Team, Inc. - All rights reserved.
  * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
- * @since       3.0
+ * @since       3.8
  */
 
 // No direct access
@@ -21,55 +21,36 @@ JHtml::_('behavior.keepalive');
 
 <form action="<?php JRoute::_('index.php?option=com_fabrik'); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
 
-	<div class="row-fluid">
+	<div class="row-fluid" id="visualizationFormTable">
 
-		<div class="span6">
-			<fieldset class="form-horizontal">
-				<legend><?php echo FText::_('COM_FABRIK_DETAILS'); ?></legend>
-				<?php foreach ($this->form->getFieldset('details') as $this->field) :
-					echo $this->loadTemplate('control_group');
-				endforeach;
-				?>
-			</fieldset>
+		<div class="span2">
+
+			<ul class="nav nav-list">
+				<li class="active">
+			    	<a data-toggle="tab" href="#tab-details">
+			    		<?php echo FText::_('COM_FABRIK_DETAILS')?>
+			    	</a>
+			    </li>
+			    <li>
+			    	<a data-toggle="tab" href="#tab-publishing">
+			    		<?php echo FText::_('COM_FABRIK_PUBLISHING')?>
+			    	</a>
+			    </li>
+			    <li>
+			    	<a data-toggle="tab" href="#tab-options">
+			    		<?php echo FText::_('COM_FABRIK_OPTIONS')?>
+			    	</a>
+			    </li>
+			</ul>
 		</div>
 
-		<div class="span5">
-			<div class="offset2">
-				<fieldset class="form-horizontal">
-						<legend>
-							<?php echo FText::_('COM_FABRIK_GROUP_LABEL_PUBLISHING_DETAILS');?>
-						</legend>
-					<?php foreach ($this->form->getFieldset('publishing') as $this->field) :
-						echo $this->loadTemplate('control_group');
-					endforeach;
-					?>
-				</fieldset>
-
-				<fieldset class="form-horizontal">
-						<legend>
-							<?php echo FText::_('COM_FABRIK_VISUALIZATION_LABEL_VISUALIZATION_DETAILS');?>
-						</legend>
-					<?php foreach ($this->form->getFieldset('more') as $this->field) :
-						echo $this->loadTemplate('control_group');
-					endforeach;
-					?>
-				</fieldset>
-			</div>
+		<div class="span10 tab-content">
+			<?php
+			echo $this->loadTemplate('details');
+			echo $this->loadTemplate('publishing');
+			echo $this->loadTemplate('options');
+			?>
 		</div>
-	</div>
-	<div class="row-fluid">
-
-		<div class="span12">
-		<fieldset class="form-horizontal">
-		    	<legend>
-		    		<?php echo FText::_('COM_FABRIK_OPTIONS');?>
-		    	</legend>
-			</fieldset>
-			<div id="plugin-container">
-				<?php echo $this->pluginFields;?>
-			</div>
-		</div>
-
 	</div>
 
 	<input type="hidden" name="task" value="" />
---
+++ administrator/components/com_fabrik/views/visualization/tmpl/bootstrap_details.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Admin Visualization Edit Tmpl
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  Fabrik
+ * @copyright   Copyright (C) 2005-2018  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @since       3.8
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+?>
+<div class="tab-pane active" id="tab-details">
+	<fieldset class="form-horizontal">
+		<div class="span6">
+			<legend><?php echo FText::_('COM_FABRIK_DETAILS'); ?></legend>
+			<?php foreach ($this->form->getFieldset('details') as $this->field) :
+				echo $this->loadTemplate('control_group');
+			endforeach;	?>
+		</div>
+	</fieldset>
+
+	<fieldset class="form-horizontal">
+		<div id="plugin-container">
+			<?php echo $this->pluginFields;?>
+		</div>
+	</fieldset>
+</div>
