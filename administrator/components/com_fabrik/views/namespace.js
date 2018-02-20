91134dd5be920866fd5b24f2e2c0d58093145231
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2018-02-20 15:19:03 +0000

    On Joomla subform-repeatable add row, run "cloned"

--- administrator/components/com_fabrik/views/namespace.js
+++ administrator/components/com_fabrik/views/namespace.js
@@ -12,7 +12,7 @@
 	FabrikAdmin = {};
 
 	// Various Joomla element plugins used to control JForm elements
-	FabrikAdmin.model = {'fields': {'fabriktable': {}, 'element': {}}};
+	FabrikAdmin.model = {'fields': {'element': {}}};
 
 	// Function to apply tips to page, after ajax call has loaded a plugin's form
 	FabrikAdmin.reTip = function () {
@@ -25,7 +25,7 @@
 			}
 		});
 		var JTooltips = new Tips($$('.hasTip'), { maxTitleChars: 50, fixed: false});
-		
+
 		// Joomla3.2
 		if (typeof(jQuery) !== 'undefined') {
 			jQuery('.hasTooltip').tooltip({'html': true, 'container': 'body'});
@@ -37,7 +37,6 @@
 }());
 
 if (typeof(jQuery) !== 'undefined') {
-
 	// Relay radio button group clicks for content added via ajax calls
 	(function ($) {
 		$(document).on('click', '.btn-group label:not(.active)', null, function (event) {
@@ -53,8 +52,38 @@ if (typeof(jQuery) !== 'undefined') {
 					label.addClass('active btn-success');
 				}
 				input.prop('checked', true);
-                input.trigger('change');
+				input.trigger('change');
 			}
 		});
 	})(jQuery);
+}
+
+// On Joomla repeatable subform add events, run elements cloned
+if (typeof(jQuery) !== 'undefined') {
+	// Relay radio button group clicks for content added via ajax calls
+	jQuery(document).on('subform-row-add', function(event, row) {
+		var base = row.getAttribute('data-base-name');
+		var group = row.getAttribute('data-group');
+		if (!group.startsWith(base)) {
+			fconsole('Fabrik namespace: new subform row, but data-group=', group, 'does not start with data-base-name=', base);
+			return;
+		}
+		var idx = group.slice(base.length);
+		row.getElements('input, select, textarea').each(function (i) {
+			if (i.id) {
+				var newId = i.id;
+				var oldId = newId.replace(idx + '__', 'X__');
+				$H(FabrikAdmin.model.fields).each(function (plugins, type) {
+					// We need to locate either lower numbered subform or use X_ if zero
+					if (typeOf(FabrikAdmin.model.fields[type][oldId]) !== 'null') {
+						var newPlugin = Object.clone(FabrikAdmin.model.fields[type][oldId]);
+						if (newPlugin !== false) {
+							newPlugin.cloned(newId, idx);
+							FabrikAdmin.model.fields[type][newId] = newPlugin;
+						}
+					}
+				});
+			}
+		});
+	});
 }
