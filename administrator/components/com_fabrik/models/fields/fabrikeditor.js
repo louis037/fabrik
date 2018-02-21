0605092e984ce10d43aaafbae97115f2446e8231
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2018-02-21 14:12:37 +0000

    Update copyrights

--- administrator/components/com_fabrik/models/fields/fabrikeditor.js
+++ administrator/components/com_fabrik/models/fields/fabrikeditor.js
@@ -1,8 +1,8 @@
 /**
  * Admin Fabrik Ace Editor
  *
- * @copyright: Copyright (C) 2005-2013, fabrikar.com - All rights reserved.
- * @license:   GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @copyright   Copyright (C) 2005-2018  Media A-Team, Inc. - All rights reserved.
+ * @license     GNU/GPL http://www.gnu.org/copyleft/gpl.html
  */
 
 /*jshint mootools: true */
