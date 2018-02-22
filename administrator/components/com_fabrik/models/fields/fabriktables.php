14a2d577cfbbf35253fdb0e3c1f64b4af4802d18
Sophist <3001893+Sophist-UK@users.noreply.github.com>
2018-02-22 21:18:29 +0000

    Move other field id generation for subform-repeat
    to be a helper function.

--- administrator/components/com_fabrik/models/fields/fabriktables.php
+++ administrator/components/com_fabrik/models/fields/fabriktables.php
@@ -91,7 +91,7 @@ class JFormFieldFabrikTables extends JFormFieldList
 
 	protected function getInput()
 	{
-		$connectionDd       = 'jform_' . $this->getAttribute('observe', $this->getAttribute('connection'));
+		$connectionDd       = $this->getAttribute('observe', $this->getAttribute('connection'));
 		$connectionInRepeat = Worker::toBoolean($this->getAttribute('connection_in_repeat', 'true'), true);
 		$script             = array();
 
@@ -102,64 +102,16 @@ class JFormFieldFabrikTables extends JFormFieldList
 
 		if ($connectionDd != '' && !array_key_exists($this->id, $fabrikTables))
 		{
-			// Do not adjust connection parameter if connection_in_repeat==false
-			if ($connectionInRepeat) {
-
-				if ($this->form->repeat)
-				{
-					// In repeat fieldset/group
-					$repeatCounter = empty($this->form->repeatCounter) ? 0 : $this->form->repeatCounter;
-					$connectionDd = $connectionDd . '-' . $repeatCounter;
-				}
-				else
-				{
-					/** Joomla Subform Repeat
-					*
-					* Joomla Subform Repeat does NOT use -x as a suffix, but instead uses the form joomla_params__subform__subformN__fieldname
-					* where the N is the repeat counter.
-					* $this->id will have the N replaced once by X for the SubForm template, and then with 0, 1 etc. for php rendered instances.
-					*
-					* Algorithm:
-					* 1. See if tables id has Joomla subForm format of repeats and if so...
-					* 2. Extract repeatCounter
-					* 3. See if connection field has common repeat prefix...
-					* 3a. Of so, insert or replace the repeatCounter from the tables id
-					* 3b. If not, use the id prefix (so for Joomla subforms user can use the connection field name without any prefix)
-					**/
-					$subForm = 0;
-					$idParts = explode('__', $this->id);
-					for ($i = count($idParts) - 1; $i > 0; $i--)
-					{
-						if ($idParts[$i - 1] === substr($idParts[$i], 0, strlen($idParts[$i - 1])))
-						{
-							$idx = substr($idParts[$i], strlen($idParts[$i - 1]));
-							if ($idx === 'X' || (is_numeric($idx) && is_int(0 + $idx)))
-							{
-								$subForm = true;
-								$newParts = array_merge(array_slice($idParts, 0, $i),array($idParts[$i - 1]));
-								$idPrefix = implode('__', $newParts);
-								if (substr($connectionDd, 0, strlen($idPrefix)) === $idPrefix)
-								{
-									$connectionDd = $idPrefix . $idx . '__' . (explode('__', substr($connectionDd, strlen($idPrefix)), 2)[1]);
-								}
-								else
-								{
-									$connectionDd = $idPrefix . $idx . '__' . substr($connectionDd, 6); // remove 'jform_' we added earlier
-								}
-								break;
-							}
-						}
-					}
-				}
-			}
+			$repeatCounter = $this->form->repeat ? (empty($this->form->repeatCounter) ? 0 : $this->form->repeatCounter) : '';
+			$connectionDd = Html::getFormRepeatId($connectionDd, $connectionInRepeat, $this->id, $repeatCounter);
 
 			$opts           = new stdClass;
-			// Following lines commented out because they are not used in fabriktablkes.js
+			// Following lines commented out because they are not used in fabriktables.js
 			// $opts->livesite = COM_FABRIK_LIVESITE;
 			// $opts->container     = 'test';
 			// $opts->inRepeatGroup = $this->form->repeat;
 			// $opts->repeatCounter = $repeatCounter;
-			$opts->conn          = $connectionDd;
+			$opts->conn          = 'jform_' . $connectionDd;
 			$opts->connInRepeat  = $connectionInRepeat;
 			$opts->value         = $this->value;
 			$opts                = json_encode($opts);
